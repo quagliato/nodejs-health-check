5fdf1d04680a15ee498f387982ee41d37a93293b
Eduardo Quagliato <eduardo@quagliato.me>
2019-06-29 18:00:22 -0300

    :hammer: refactoring code to promise-based processing

--- check.js
+++ check.js
@@ -1,87 +1,85 @@
-// check.js
-// It checks URLs to see if they are up and running and notifies when it's down
-// Author: Eduardo Quagliato <eduardo@quagliato.me>
-// MIT License
+const fs = require('fs')
+const nodemailer = require('nodemailer')
+const smtpTransport = require('nodemailer-smtp-transport')
+const got = require('got')
 
-// Dependencies
-var fs      = require("fs");
-var nodemailer = require("nodemailer");
-var request = require("request");
-var smtpTransport = require('nodemailer-smtp-transport');
-
-// It validates the configurations and triggers the requests to every target
-// specified in the configuration file.
-var processConfig = function(config){
-  if (typeof config !== "object") {
-    console.log(new Date().toISOString() + " The config file is not a array.");
-  } else if (config.length === 0) {
-    console.log(new Date().toISOString() + " The config file is empty.");
-  } else {
-    var master_smtp_settings = false;
-    if (config.hasOwnProperty("smtp_settings")) {
-      master_smtp_settings = config.smtp_settings;
-    }
-
-    if (config.hasOwnProperty("targets")) {
-      if (config.targets.length === 0) {
-        console.log(new Date().toISOString() + " No targets in the config");
-      } else {
-        for (var i = 0; i < config.targets.length; i++) {
-          var target = config.targets[i];
-
-          if (master_smtp_settings === false && !target.hasOwnProperty("smtp_settings")) {
-            console.log(new Date().toISOString() + " No master SMTP settings and no target's SMTP settings.");
-            return;
-          }
-
-          requestToTarget(target, master_smtp_settings);
-        }
-      }
-    }
-  }
-};
-
-// This functions requests the specified body to the specified target with the
-// specified method and process it result. If the result is not the expected,
-// it sends and notification message to the e-mail configurated.
-var requestToTarget = function(target, master_smtp_settings){
-  var requestInfo = {
-    "url" : target.url,
-    "method" : target.method,
-    "body" : target.body,
-    "json": true
-  };
+const run = async (config) => {
+  const {
+    targets
+    master_smtp_settings
+  } = processConfig(config)
   
-  request(requestInfo, function(error, req, body){
-    var parseTry = false;
-    try {
-      if (typeof body === "string") {
-        parseTry = JSON.parse(body);
-      } else if (typeof body === "object") {
-        parseTry = JSON.stringify(body);
+  return await testTargets(targets)
+    .then(filterResponses)
+    .then(responses => sendAlerts(responses, master_smtp_settings))
+}
+
+const processConfig = (config) => {
+  try {
+    JSON.parse(config)
+  } catch {
+    console.log(`${new Date().toISOString()} Config information is not a valid JSON`)
+    process.exit(1)
+  }
+  
+  const configObject = JSON.parse(config)
+
+  if (!configObject 
+    || !configObject.smtp_settings
+    || !configObject.targets 
+    || configObject.targets.length === 0) {
+    console.log(`${new Date().toISOString()} Not enough configuration to proceed. Please check the targets and the SMTP settings.`)
+    process.exit(1)
+  }
+  
+  return {
+    targets: configObject.targets,
+    master_smtp_settings: configObject.smtp_settings
+  } 
+}
+
+const testTargets = (targets) => Promise.all(targets.map(target => requestToTarget(target)))
+
+const filterResponses = (responses) => responses.filter(response !== false)
+
+const sendAlerts = (responses, master_smtp_settings) => Promise.all(responses.map((response) => {
+  return new Promise({
+    const target = response.target
+    const subject = `${new Date().toISOString()} ${target.app_name} is down!`
+    const content = `${subject}\nStatus Code: ${response.statusCode}\nError: ${body}`
+    return sendMail(target.report_email,
+      subject,
+      target.smtp_settings || master_smtp_settings,
+      content)
+  })
+}))
+
+const requestToTarget = (target) => {
+  if (!target.url) throw new Error('URL is mandatory')
+  const requestOpts = {
+    url: target.url,
+    method : target.method || 'GET',
+    body : target.body,
+    json: true
+  }
+  
+  return got(requestOpts)
+    .then(response => {
+      console.log(`${new Date().toISOString()} is ok!`)
+      return false
+    })
+    .catch(response => {
+      return {
+        target,
+        statusCode: response.statusCode,
+        body: response.error
       }
-    } catch (e) {
-      parseTry = false;
-    }
+    })
+}
 
-    if (error || req.statusCode !== 200) {
-      var emailContent = new Date().toISOString() + " " + target.app_name + " is down!";
-      if (req && req.statusCode) emailContent += "\n" + "Status Code: " + req.statusCode;
-      if (error) emailContent += "\n" + "Error: " + JSON.stringify(error);
-      
-      console.log(emailContent);
+const sendMail = (to, subject, smtp_settings, content) => {
 
-      sendMail(target.report_email, target.app_name + " is down!", master_smtp_settings !== false ? master_smtp_settings : target.smtp_settings, emailContent);
-    } else {
-      console.log(new Date().toISOString() + " " + target.app_name + " is ok!");
-    }
-  });
-};
-
-// sendMail kind of explain itself, right?
-var sendMail = function(to, subject, smtp_settings, content){
-
-  var transporter = nodemailer.createTransport(smtpTransport({
+  const transporter = nodemailer.createTransport(smtpTransport({
     host: smtp_settings.host,
     port: smtp_settings.port,
     ignoreTLS: false,
@@ -89,47 +87,54 @@ var sendMail = function(to, subject, smtp_settings, content){
       user: smtp_settings.user,
       pass: smtp_settings.pass
     }
-  }));
+  }))
 
-  while (content.indexOf("\n") >= 0) {
-    content = content.replace("\n", "<br>");
-  }
+  const htmlContent = content.replace(/\\n/g)
 
-  var mailOptions = {
+  const mailOpts = {
     to: to, // list of receivers
     subject: subject, // Subject line
-    text: subject, // plaintext body
-    html: content || ("<p>" + subject + "</p>") // html body
+    text: content, // plaintext body
+    html: htmlContent// html body
   };
 
-  transporter.sendMail(mailOptions, function(error, info){
-    if(error){
-      return console.log(error);
-    }
-    console.log(new Date().toISOString() + ' "' + subject + '" is sent: ' + info.response);
-  });
-};
-
-
-// This is the actual start of this script. It tries to load the configuration
-// from a environment variable or from a configuration file (which also has to
-// be setted up with a environment variable).
-if (process.env.CONFIG) {
-  processConfig(JSON.parse(process.env.CONFIG));
-} else if (process.env.CONFIG_FILE) {
-  fs.readFile(process.env.CONFIG_FILE, function(err, data){
-    if (err) {
-      console.log(new Date().toISOString() + " Could not load CONFIG_FILE");
-      console.log(err);
-      process.exit(1);
-    } else {
-      var config = JSON.parse(data);
-      processConfig(config);
-    }
-  });
-} else {
-  console.log(new Date().toISOString() + " No config setted");
-  process.exit(1);
+  return transporter.sendMail(mailOpts)
+    .then(info => {
+      console.log(`${new Date().toISOString()} "${subject}" is sent: ${info.response}`)
+    })
 }
+
+const readConfigFile = (path) => {
+  if (!fs.lstatSync(path)) {
+    console.log(`${new Date().toISOString()} ${path} does not exist.`);
+    return false
+  }
+  
+  return fs.readFileSync(path)
+}
+
+const readConfigFileFromURL = (url) => {
+  return got(url)
+    .then(response => response)
+    .catch(() => {
+      console.log(`${new Date().toISOString()} Could not find config file in ${url}`)
+      return false;
+    })
+}
+
+async function start() {
+  if (process.env.HEALTH_CHECK_CONFIG) {
+    return run(process.env.HEALTH_CHECK_CONFIG)
+  } else if (process.env.HEALTH_CHECK_CONFIG_FILE) {
+    return run(readConfigFile(process.env.HEALTH_CHECK_CONFIG_FILE))
+  } else if (process.env.HEALTH_CHECK_CONFIG_URL) {
+    return run(await readConfigFileFromURL(process.env.HEALTH_CHECK_CONFIG_URL))
+  } else {
+    console.log(`${new Date().toISOString()} No config set.`);
+    process.exit(1);
+  }
+}
+
+start()
 
 // That's all, folks!
